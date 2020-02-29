b7bb2454b4950c1ecb348de64514a14f5ce64e07
saraswathicv <45695148+saraswathicv@users.noreply.github.com>
2020-02-29 20:06:34 +0530

    change default value

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,13 +5,13 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-2"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-2 = "ami-0a1a642a14b53d32b"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-0d729a60"
   }
